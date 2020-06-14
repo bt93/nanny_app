9905d7d088b7a8b39168c9a36fff0b8e0b6a84eb
Jason Howie <howiejason93@gmail.com>
2020-06-14 09:17:22 -0400

    Start Database Script

---
+++ db/init_script.sql
@@ -0,0 +1,16 @@
+-- Make sure not using other db's
+USE master
+GO
+
+-- Drop the existing db if it already exists
+DROP DATABASE IF EXISTS NannyDB
+
+-- Create new database
+CREATE DATABASE NannyDB
+GO
+
+USE NannyDB
+GO
+
+
+

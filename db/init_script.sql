cb4710335b064e15fe1da02ee55a8bea86bc3284
Jason Howie <howiejason93@gmail.com>
2020-06-14 11:10:05 -0400

    Fix init db script

--- db/init_script.sql
+++ db/init_script.sql
@@ -23,11 +23,64 @@ CREATE TABLE address (
 	country NVARCHAR(25),
 )
 
+CREATE TABLE caretaker (
+	caretaker_id INT IDENTITY PRIMARY KEY,
+	address_id INT NOT NULL,
+	first_name NVARCHAR(80) NOT NULL,
+	last_name NVARCHAR(80) NOT NULL,
+	email NVARCHAR(120) NOT NULL,
+	password NVARCHAR (60) NOT NULL,
+	phone_number NVARCHAR(20) NOT NULL,
+	CONSTRAINT fk_caretaker_address FOREIGN KEY (address_id) REFERENCES address(address_id)
+)
+
+CREATE TABLE parent (
+	parent_id INT IDENTITY PRIMARY KEY,
+	address_id INT NOT NULL,
+	first_name NVARCHAR(80) NOT NULL,
+	last_name NVARCHAR(80) NOT NULL,
+	email NVARCHAR(120) NOT NULL,
+	phone_number NVARCHAR(20) NOT NULL
+	CONSTRAINT fk_parent_address FOREIGN KEY (address_id) REFERENCES address(address_id)
+)
+
+CREATE TABLE child (
+	child_id INT IDENTITY PRIMARY KEY,
+	caretaker_id INT NOT NULL,
+	first_name NVARCHAR(80) NOT NULL,
+	last_name NVARCHAR(80) NOT NULL,
+	gender NVARCHAR(11),
+	date_of_birth DATE,
+	rate_per_hour MONEY NOT NULL,
+	needs_diapers BIT NOT NULL,
+	CONSTRAINT fk_child_caretaker FOREIGN KEY (caretaker_id) REFERENCES caretaker(caretaker_id)
+)
+
+CREATE TABLE child_parent (
+	child_id INT NOT NULL,
+	parent_id INT NOT NULL,
+	CONSTRAINT pk_child_parent PRIMARY KEY (child_id, parent_id),
+	CONSTRAINT fk_child FOREIGN KEY (child_id) REFERENCES child(child_id),
+	CONSTRAINT fk_parent FOREIGN KEY (parent_id) REFERENCES parent(parent_id)
+)
+
+CREATE TABLE session (
+	session_id INT IDENTITY PRIMARY KEY,
+	child_id INT NOT NULL,
+	drop_off DATETIME,
+	pick_up DATETIME,
+	total_hours INT,
+	total_pay MONEY,
+	notes TEXT
+	CONSTRAINT fk_session_child FOREIGN KEY (child_id) REFERENCES child(child_id)
+)
+
 CREATE TABLE diaper (
 	diaper_id INT IDENTITY PRIMARY KEY,
 	session_id INT NOT NULL,
 	time DATETIME NOT NULL,
 	notes TEXT
+	CONSTRAINT fk_diaper_session FOREIGN KEY (session_id) REFERENCES session(session_id)
 )
 
 CREATE TABLE meal (
@@ -36,6 +89,7 @@ CREATE TABLE meal (
 	time DATETIME NOT NULL,
 	type NVARCHAR(20),
 	notes TEXT
+	CONSTRAINT fk_meal_session FOREIGN KEY (session_id) REFERENCES session(session_id)
 )
 
 CREATE TABLE nap (
@@ -44,4 +98,5 @@ CREATE TABLE nap (
 	start_time DATETIME NOT NULL,
 	end_time DATETIME NOT NULL,
 	notes TEXT
+	CONSTRAINT fk_nap_session FOREIGN KEY (session_id) REFERENCES session(session_id)
 )

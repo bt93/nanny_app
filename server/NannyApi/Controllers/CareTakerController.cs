d84bd5e5cf08703efccaf1615f253b070debd533
Jason Howie <howiejason93@gmail.com>
2020-06-24 17:50:37 -0400

    Add Gets for Caretakers

--- server/NannyApi/Controllers/CareTakerController.cs
+++ server/NannyApi/Controllers/CareTakerController.cs
@@ -4,19 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using NannyApi.DAL;
+using NannyApi.Models;
 
 namespace NannyApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CareTakerController : ControllerBase
+    public class CaretakersController : ControllerBase
     {
+        private IConfiguration configuration;
         private CareTakerSqlDAO careTakerDao;
 
-        public CareTakerController()
+        public CaretakersController(IConfiguration config)
         {
-            
+            configuration = config;
+            careTakerDao = new CareTakerSqlDAO(configuration.GetConnectionString("NannyDB"));
+        }
+
+        // Get for api/caretakers
+        [HttpGet]
+        public ActionResult<IList<CareTaker>> GetCareTakers()
+        {
+            return Ok(careTakerDao.GetAllCareTakers());
+        }
+
+        // Get for api/caretakers/:id
+        [HttpGet("{id}")]
+        public ActionResult<CareTaker> GetCareTaker(int id)
+        {
+            CareTaker careTaker = careTakerDao.GetCareTakerById(id);
+
+            if (careTaker != null)
+            {
+                return careTaker;
+            }
+
+            return NotFound();
         }
     }
 }
--- server/NannyApi/Models/CareTaker.cs
+++ server/NannyApi/Models/CareTaker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace NannyApi.Models
@@ -8,16 +9,27 @@ namespace NannyApi.Models
     {
         public int CareTakerId { get; set; }
         public int AddressId { get; set; }
+        [Required(ErrorMessage = "The 'First Name field is required.")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Required(ErrorMessage = "The 'Last Name field is required.")]
         public string EmailAddress { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "The 'Email Address' field is required.")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "The 'Password' field is required.")]
         public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "The 'Phone Number' field is required.")]
         public string Street { get; set; }
+        [Required(ErrorMessage = "The 'Street' field is required.")]
         public string City { get; set; }
+        [Required(ErrorMessage = "The 'City' field is required.")]
         public string State { get; set; }
+        [Required(ErrorMessage = "The 'State' field is required.")]
         public int Zip { get; set; }
+        [Required(ErrorMessage = "The 'Zip Code' field is required.")]
         public string County { get; set; }
+        [Required(ErrorMessage = "The 'Country' field is required.")]
         public string Country { get; set; }
 
         public string GetAddress()

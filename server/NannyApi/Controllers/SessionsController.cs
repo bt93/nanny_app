53cd8044c6a530d915a52216389219bee6ebd68b
Jason Howie <howiejason93@gmail.com>
2020-08-02 12:44:52 -0400

    Added Comments

--- server/NannyApi/Controllers/SessionsController.cs
+++ server/NannyApi/Controllers/SessionsController.cs
@@ -31,7 +31,7 @@ namespace NannyApi.Controllers
                 return 0;
             }
         }
-
+        // TODO: Comment all the endpoints and edit README
         private ISessionDAO sessionDao;
         private IChildDAO childDao;
         private IDiaperDAO diaperDao;
@@ -50,8 +50,11 @@ namespace NannyApi.Controllers
             this.parentDao = parentDao;
         }
 
-        // /api/sessions
-        // Will get a full list of the current session in progress now
+        /// <summary>
+        /// GET /api/sessions
+        /// Will get a full list of the current session in progress now
+        /// </summary>
+        /// <returns>List of Sessions</returns>
         [HttpGet]
         public ActionResult<List<Session>> GetCurrentSessions()
         {
@@ -71,6 +74,12 @@ namespace NannyApi.Controllers
             return Ok(sessions);
         }
 
+        /// <summary>
+        /// GET /api/sessions/{sessionId}
+        /// Gets all the sessions by id
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <returns>Session</returns>
         [HttpGet("{sessionId}")]
         public ActionResult<Session> GetSessionByID(int sessionId)
         {
@@ -91,6 +100,12 @@ namespace NannyApi.Controllers
             return Ok(session);
         }
 
+        /// <summary>
+        /// GET /api/sessions/child/{childId}
+        /// Gets a list of the sessions by child
+        /// </summary>
+        /// <param name="childId"></param>
+        /// <returns>List of Sessions</returns>
         [HttpGet("child/{childId}")]
         public ActionResult<List<Session>> GetAllSessionsByChild(int childId)
         {
@@ -120,6 +135,11 @@ namespace NannyApi.Controllers
             return Ok(sessions);
         }
 
+        /// <summary>
+        /// GET /api/sessions/all
+        /// Returns a list of all the sessions in by caretaker
+        /// </summary>
+        /// <returns>List of sessions</returns>
         [HttpGet("all")]
         public ActionResult<List<Session>> GetAllSessions()
         {
@@ -139,6 +159,12 @@ namespace NannyApi.Controllers
             return Ok(sessions);
         }
 
+        /// <summary>
+        /// GET /api/sessions/{sessionId}/naps
+        /// Gets a list of all the naps per session
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <returns>List of Naps</returns>
         [HttpGet("{sessionId}/naps")]
         public ActionResult<List<Nap>> GetAllNaps(int sessionId)
         {
@@ -153,6 +179,13 @@ namespace NannyApi.Controllers
             return Ok(naps);
         }
 
+        /// <summary>
+        /// GET /api/sessions/naps/{napId}
+        /// Gets a Nap by the Id
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <param name="napId"></param>
+        /// <returns>Nap</returns>
         [HttpGet("{sessionId}/naps/{napId}")]
         public ActionResult<Nap> GetNapById(int sessionId, int napId)
         {
@@ -173,6 +206,13 @@ namespace NannyApi.Controllers
             return Ok(nap);
         }
 
+        /// <summary>
+        /// POST /api/sessions/child/{childId] 
+        /// Creates a new session
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="childId"></param>
+        /// <returns>Session</returns>
         [HttpPost("child/{childId}")]
         public ActionResult<Session> CreateSession(Session session, int childId)
         {
@@ -181,6 +221,13 @@ namespace NannyApi.Controllers
             return Created($"/api/sessions/{newSession.SessionId}", newSession);
         }
 
+        /// <summary>
+        /// POST /api/sessions/{sessionId}/naps
+        /// Post a new nap given a session
+        /// </summary>
+        /// <param name="nap"></param>
+        /// <param name="sessionId"></param>
+        /// <returns>Nap</returns>
         [HttpPost("{sessionId}/naps")]
         public ActionResult<Nap> CreateNap(Nap nap, int sessionId)
         {
@@ -196,6 +243,13 @@ namespace NannyApi.Controllers
             return Created($"/api/sessions/{sessionId}/naps/{newNap.NapId}", newNap);
         }
 
+        /// <summary>
+        /// PUT /api/sessions/end/{sessionId}
+        /// Ends a session given an id and session
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="sessionId"></param>
+        /// <returns>Session</returns>
         [HttpPut("end/{sessionId}")]
         public ActionResult<Session> EndSession(Session session, int sessionId) 
         {
@@ -209,6 +263,13 @@ namespace NannyApi.Controllers
             return Created($"/api/sessions/{endSession.SessionId}", endSession);
         }
 
+        /// <summary>
+        /// PUT /api/sessions/{sessionId}
+        /// Updates a session in progress
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="sessionId"></param>
+        /// <returns>Session</returns>
         [HttpPut("{sessionId}")]
         public ActionResult<Session> UpdateCurrentSession(Session session, int sessionId)
         {

778f3314f381227a75fa718695164ae52095df12
Jason Howie <howiejason93@gmail.com>
2021-10-02 11:07:56 -0400

    created stored procedure helper methods

---
+++ server/NannyApi/DAL/DBHelpers/StoredProcedureHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NannyApi.DAL.DBHelpers
+{
+    public class StoredProcedureHelper
+    {
+        private SqlConnection _connection;
+
+        public StoredProcedureHelper(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public SqlCommand CreateCommand(string commandName)
+        {
+            var command = new SqlCommand(commandName, _connection);
+            command.CommandType = CommandType.StoredProcedure;
+            return command;
+        }
+
+        public SqlParameter AddWithValue(string parameterName, object value, SqlDbType DBType)
+        {
+            var parameter = new SqlParameter(parameterName, value);
+            parameter.SqlDbType = DBType;
+            return parameter;
+        }
+
+        public object Single(SqlCommand command)
+        {
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+
+            var result = command.ExecuteScalar();
+            command.Connection.Close();
+            return result;
+        }
+        
+        public SqlDataReader ExecuteReader(SqlCommand command)
+        {
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+
+            var result = command.ExecuteReader();
+            command.Connection.Close();
+
+            return result;
+        } 
+
+        public int ExecuteNonQuery(SqlCommand command)
+        {
+            if (command.Connection.State != ConnectionState.Open)
+            {
+                command.Connection.Open();
+            }
+
+            var result = command.ExecuteNonQuery();
+            command.Connection.Close();
+            return result;
+        }
+    }
+}
